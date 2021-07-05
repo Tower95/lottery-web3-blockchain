498e670ed13f018b9abd25869bf320e57330ef60
Tower95 <torrestmi3@gmail.com>
2021-07-04 19:37:10 -0700

    first Commit

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  'lawsuit dumb spider dizzy flag cook jazz wave clip fire juice banner',
+  'https://rinkeby.infura.io/v3/75f320976e904cb780a140fe348d1497'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
